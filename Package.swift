54cbc32de8173de64b7740dbb298dc752ab98934
Donadio Juan <juan.donadio@pixegon.com>
2026-08-26 09:23:59 -0300

    chore: upgrade to Capacitor 8

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Tmtplayer",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "Tmtplayer",
